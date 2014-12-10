a9d3be66dc91ad732f28965dfb00ebd0f711d9e3
Richa Arora <ricarora@gmail.com>
2014-12-10 12:03:52 -0800

    add sign-up body content rspec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -10,5 +10,11 @@ RSpec.describe UserMailer, :type => :mailer do
       expect(mailer.to).to include "b@example.com"
       # expect(mailer.to.first).to eq ""
     end
+
+    it "includes the username within the email body" do
+      user = User.create(email: "b@example.com")
+      mailer = UserMailer.sign_up(user.id)
+      expect(mailer.body.to_s).to match(/Hello b@example.com/)
+    end
   end
 end
